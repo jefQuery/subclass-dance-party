c5c7aad95226d0be1be7e59be1281fecb6abdc79
jefQuery <jeffrey.milberger@gmail.com>
2017-02-13 16:50:56 -0800

    Changed classes to be pseudoclassical. Passes all tests but doesn't work on browser.

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,7 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new MakeBlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -14,7 +14,7 @@ describe('blinkyDancer', function() {
 
   it('should have a step function that makes its node blink', function() {
     sinon.spy(blinkyDancer.$node, 'toggle');
-    blinkyDancer.step();
+    blinkyDancer.step(timeBetweenSteps);
     expect(blinkyDancer.$node.toggle.called).to.be.true;
   });
 
